67826d6757d2658e699db049c7377c325e25f4aa
cupcakegirl2026 <liamnyaks@gmail.com>
2026-09-19 09:44:47 +0300

    Add files via upload

---
+++ mane.py
@@ -0,0 +1,8 @@
+temp=int(input("Please enter the temperature outside: "))
+if temp <1:
+    outfit="Jaket"
+    print(f"Its cold today.Wear a {outfit}")
+
+else:
+    outfit="T-shirt"
+    print(f"Its warm today.Wear a {outfit}")
